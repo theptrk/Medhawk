7b1146783abb75fbd570b3fa85c9ea943a6496d8
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-24 08:41:51 -0700

    Refactored effects handlers to use new patterns.

--- server/controllers/effect.js
+++ server/controllers/effect.js
@@ -1,17 +1,51 @@
-var Drug = require('../models').Drug;
+var Models          = require('../models');
+var Drug            = Models.Drug;
+var Effect          = Models.Effect;
+var controllerUtils = require('./controllerUtils.js');
+var Q               = require('q');
 
-module.exports.getEffects = function (req, res) {
+module.exports.getEffects = controllerUtils.robustQuery(Effect, {
+  matching: {},
+  fields: "name",
+  options: {}
+});
+
+module.exports.getEffectsFromDrug = function (req, res) {
   var data = req.body;
 
-  Drug.find({name: data.drugName, company: data.company})
-    .exec()
+  Q(Drug.findOne({name: data.drugName})
+    .exec())
     .then(function (drug) {
       return drug.populate('effects');
     })
-    .then(function (effects) {
-      res.json(effects);
-    })
-    .fail(function (err) {
-      res.send(500, err);
-    });
+    .then(res.json.bind(res))
+    .fail(controllerUtils.internalServerError(res));
+};
+
+module.exports.postEffectToDrug = function (req, res) {
+  var data = req.body;
+
+  Q.all([Effect.findOne({name: effectName}).exec(), Drug.findOne({name: data.drugName})])
+    .spread(function (effect, drug) {
+      drug.effects.push(effect._id);
+      return drug.save().exec();
+    }).then(function (saveError) {
+      if (saveError) {
+        throw saveError;
+      } else {
+        res.send(201, "Posted Data.");
+      }
+    }).fail(controllerUtils.internalServerError(res));
+};
+
+module.exports.postNewEffect = function (req, res) {
+  var data = req.body;
+
+  Q(Effect.create({name: req.name})).then(function (saveError) {
+    if (saveError) {
+      throw saveError;
+    } else {
+      res.send(201, "Posted Data.");
+    }
+  });
 };
