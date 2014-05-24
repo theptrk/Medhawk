0681aa1716199e22f1ca7d3ac78333eddf175320
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-23 20:11:22 -0700

    Added routes from controllers.

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,7 @@
-module.exports = function (app) {
+var controllers = require('/controllers');
 
+module.exports = function (app) {
+  app.get("/drugs", controllers.getDrugs);
+  app.get("/affects", controllers.getAffects);
+  app.post("/tweet", controllers.postTweet);
 };
