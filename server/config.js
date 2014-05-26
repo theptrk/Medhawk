edc9af07dc5ed664be6c1be2a1836c68544a00a1
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-26 16:09:18 -0700

    Added better loggin and removed static server.

--- server/config.js
+++ server/config.js
@@ -2,7 +2,14 @@ var express = require('express');
 var path = require('path');
 
 module.exports = function (app) {
+  app.use(function (req, res, next) {
+    console.log("Received request:");
+    require('morgan')({format: 'short', immediate: true})(req, res, next);
+  });
   app.use(require('morgan')({format: 'short'}));
   app.use(require('body-parser')());
-  app.use(express.static(path.join(__dirname + '/public')));
+  app.use(function (req, res, next) {
+    console.log("Request Body:", req.body);
+    next();
+  });
 };
