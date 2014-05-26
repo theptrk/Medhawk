6085166a271d6d82e7034e759ee842d098da0d6d
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-26 16:09:48 -0700

    Added secret to routeUtils.

--- server/routeUtils.js
+++ server/routeUtils.js
@@ -1,4 +1,5 @@
-var _ = require('lodash');
+var _      = require('lodash');
+var secret = require('./secret.js');
 
 module.exports.validateRequest = function (fields) {
   return function (req, res, next) {
