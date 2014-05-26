c760753ffbbb3853c74951d42e3203f56ba67afa
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-26 16:09:58 -0700

    Added first passing tests.

--- server/tests/tests.js
+++ server/tests/tests.js
@@ -10,12 +10,13 @@ var utils   = require('./testUtil.js');
 var request = require('request');
 
 var reqPort = 3000;
+var reqUrl = 'http://localhost:' + reqPort;
 
 describe('Server', function () {
   before(function (done) {
-    Models.Effect.create({
+    Q(Models.Effect.create({
       name: 'TestEffect'
-    })
+    }))
     .then(function (effect) {
       return Models.Drug.create({
           name: 'Test',
@@ -24,16 +25,17 @@ describe('Server', function () {
       });
     }).then(function () {
       done();
-    }, console.error.bind(console));
+    }).fail(console.error.bind(console));
   });
 
   after(function (done) {
-    Models.Drug.findOne({name: 'Test'}).exec().then(function (drug) {
-      return drug.remove().exec();
-    }).then(function () {
+    Q(Models.Drug.findOne({name: 'Test'}).exec()
+    .then(function (drug) {
+      return drug.remove();
+    })).then(function () {
       return Models.Effect.findOne({name: 'TestEffect'}).exec();
     }).then(function (effect) {
-      return effect.remove().exec();
+      return effect.remove();
     }).then(function () {
       done();
     });
@@ -41,31 +43,42 @@ describe('Server', function () {
 
   describe('/drugs', function() {
     it('should return 200 on a get request', function (done) {
+      request(reqUrl + '/drugs', {form: {appKey: "TestKey"}}, function (error, response, body) {
+        expect(!!error).to.be.false;
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
     });
 
-    it('should return the right drugs', function (done) {
+    xit('should return the right drugs', function (done) {
+
     });
 
-    it('should support robust queries', function (done) {
+    xit('should support robust queries', function (done) {
     });
   });
 
   describe('/effects', function() {
     it('should return 200 on a get request', function (done) {
+      request(reqUrl + '/effects', {form: {appKey: "TestKey"}}, function (error, response, body) {
+        expect(!!error).to.be.false;
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
     });
 
-    it('should return the right effects', function (done) {
+    xit('should return the right effects', function (done) {
     });
 
-    it('should support robust queries', function (done) {
+    xit('should support robust queries', function (done) {
     });
   });
 
   describe('/tweet', function () {
-    it('should return 201 on a post request', function (done) {
+    xit('should return 201 on a post request', function (done) {
     });
 
-    it('should post a new tweet to the database', function (done) {
+    xit('should post a new tweet to the database', function (done) {
     });
   });
 });
