bb5e94aa2882d12f805c0e721d881b39092122ed
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-27 13:05:02 -0700

    Corrected typo.

--- server/models/index.js
+++ server/models/index.js
@@ -2,5 +2,5 @@ module.exports = {
   Tweet:  require('./tweet.js').Tweet,
   Drug:   require('./drug.js').Drug,
   Effect: require('./effect.js').Effect,
-  Emoji: requite('./emoji.js').Emoji
+  Emoji: require('./emoji.js').Emoji
 };
