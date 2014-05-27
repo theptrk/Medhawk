d92af5896c4f10c1b3edcdce8a158ee9e818a021
emiidee <emily.y.dong@gmail.com>
2014-05-26 17:54:44 -0700

    Added controller and redirect for drug selection page

--- client/www/app/drugs/drugs.js
+++ client/www/app/drugs/drugs.js
@@ -1,5 +1,13 @@
-angular.module('drugs', [])
+angular.module('drugs', ['drugServices'])
 
-.controller('DrugCtrl', ['$scope', function($scope){
+.controller('DrugCtrl', ['$scope', '$rootScope', '$state', 'drugNames', 'drugEffects', function($scope, $rootScope, $state, drugNames, drugEffects){
   console.log('This is the DrugCtrl controller');
+
+  $scope.drugs = drugNames;
+  $scope.effects = drugEffects;
+
+  $scope.navEffects = function(drugName) {
+    $rootScope.drugName = drugName;
+    $state.go('drugs.effects');
+  };
 }]);
