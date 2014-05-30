ea110f4149407f77c0db4db4fc26856b5ac2ac82
theptrk <patrick.tran06@gmail.com>
2014-05-29 18:41:31 -0700

    Imprted config and twitterLib; Added basic Twitter functionality

--- client/www/app/share/share.js
+++ client/www/app/share/share.js
@@ -1,6 +1,6 @@
-angular.module('share', [])
+angular.module('share', ['config', 'twitterLib'])
 
-.controller('ShareCtrl', ['$rootScope', '$scope', function($rootScope, $scope){
+.controller('ShareCtrl', ['$rootScope', '$scope', '$http', 'TwitterLib', 'configuration', function($rootScope, $scope, $http, TwitterLib, configuration){
   var effects = _.pluck($rootScope.drugEffects, "name");
   var effectString = "";
 
@@ -21,4 +21,19 @@ angular.module('share', [])
   $scope.tweetMessage = "I'm taking " + $rootScope.drugName + " and I'm experiencing " + effectString + 
     ". @FDA " + $rootScope.drugHandle +
     " #ThisDrugSucks via @Medhawk";
+
+  $scope.doLogin = function(){
+    TwitterLib.init();
+  };
+
+  $scope.doTweet = function() {
+    TwitterLib.tweet(this.tweetMessage).then(function(_data) {
+      alert("tweet success");
+      console.log(_data);
+      alert(_data);
+
+    }, function(_error) {
+      console.log("tweet error" + JSON.stringify(_error));
+    });
+  };
 }]);
