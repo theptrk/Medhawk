5a56fe28e001493015c780485f770799bf5bdab7
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-29 22:35:12 -0700

    Added gulpfile for karma testing.

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,10 +1,25 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var bower = require('bower');
-var rename = require('gulp-rename');
 var sh = require('shelljs');
+var jshint = require('gulp-jshint');
+var karma = require('gulp-karma');
 
-gulp.task('watch', function() {
+var files = {
+  scripts: ['./www/app/**/*.js', './tests/**/*.js'],
+  tests: ['./tests/**/*.js']
+}
 
+gulp.task('lint', function () {
+  return gulp.src(files.scripts)
+    .pipe(jshint())
+    .pipe(jshint.reporter(require('jshint-stylish')))
+    .pipe(jshint.reporter('fail'));
 });
 
+gulp.task('test', ['lint'], function () {
+  return gulp.src(files.tests)
+    .pipe(karma({
+      configFile: './karma.conf.js',
+      action: 'watch'
+    })).on('error', function (err) { throw err; });
+});
