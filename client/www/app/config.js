c31c8a2ccc893cc7fbab1fbc0d748d5f93a8f16b
theptrk <patrick.tran06@gmail.com>
2014-06-02 12:59:04 -0700

    Added Twitter Auth Credentials

--- client/www/app/config.js
+++ client/www/app/config.js
@@ -3,6 +3,14 @@ angular.module('config', [])
 .factory('configuration', function (){
   return {
     SERVERPATH: 'http://localhost:3000',
-    APPKEY: 'TestKey'
+    APPKEY: 'TestKey',
+    oauthSettings: {
+      consumerKey: 'hG5JkrDkarl5cPmjc94Lwsu7a',
+      consumerSecret: '3s0oxghx1yQ21ag8r7R5KnOVWRJXuXHsjJvjCmdJRxj20FKUIv',
+      requestTokenUrl: 'https://api.twitter.com/oauth/request_token',
+      authorizationUrl: "https://api.twitter.com/oauth/authorize",
+      accessTokenUrl: "https://api.twitter.com/oauth/access_token",
+      callbackUrl: "callbackUrl"
+    }
   };
 });
