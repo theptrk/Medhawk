88ab685d1088a93c1f953dbaafd8c4f9fe2ab2a7
mikefreeman <michael.p.freeman@gmail.com>
2014-06-03 12:54:41 -0700

    Injected $state into controller

--- client/www/app/home/home.js
+++ client/www/app/home/home.js
@@ -1,6 +1,6 @@
 angular.module('home', [])
 
-.controller('HomeCtrl', ['$rootScope', '$scope', function($rootScope, $scope) {
+.controller('HomeCtrl', ['$rootScope', '$scope', '$state', function($rootScope, $scope, $state) {
   $scope.start = function() {
     $state.go('drugs.meds');
   };
