10c0921172f463cf460441fa24901180b3e0858a
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-02 19:38:20 -0700

    Removed emoji routes.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,39 +2,33 @@ var controllers = require('../controllers');
 var utils       = require('./routeUtils.js');
 
 module.exports.route = function (app) {
-  app.get("/drugs", utils.isApp, 
+  app.get("/drugs", utils.isApp,
     controllers.getDrugs);
 
-  app.get("/effects", utils.isApp, 
+  app.get("/effects", utils.isApp,
     controllers.getEffects);
 
   app.get("/tweets", utils.isApp,
     controllers.getTweets);
 
-  app.get("/emojis", utils.isApp,
-    controllers.getEmoji);
-
-  app.post("/effects/postToDrug", utils.isApp, 
-    utils.validateRequest(["drugName", "effectName"]), 
+  app.post("/effects/postToDrug", utils.isApp,
+    utils.validateRequest(["drugName", "effectName"]),
     controllers.postEffectToDrug);
 
-  app.get("/effects/fromDrug", utils.isApp, 
-    utils.validateRequest(["drugName"]), 
+  app.get("/effects/fromDrug", utils.isApp,
+    utils.validateRequest(["drugName"]),
     controllers.getEffectsFromDrug);
 
-  app.post("/drugs/post", utils.isApp, 
-    utils.validateRequest(["name", "company", "handle"]), 
+  app.post("/drugs/post", utils.isApp,
+    utils.validateRequest(["name", "company", "handle"]),
     controllers.postDrug);
-  
-  app.post("/effects/post", utils.isApp, 
+
+  app.post("/effects/post", utils.isApp,
     utils.validateRequest(["name"]),
     controllers.postEffect);
 
-  app.post("/tweets/post", utils.isApp, 
-    utils.validateRequest(["tweet", "link"]), 
+  app.post("/tweets/post", utils.isApp,
+    utils.validateRequest(["tweet", "link"]),
     controllers.postTweet);
 
-  app.post("/emojis/post", utils.isApp, 
-    utils.validateRequest(["symptom", "link"]), 
-    controllers.postEmoji);
 };
