68150a2fd2a386cf73584da0321dcfbff3548cdd
mikefreeman <michael.p.freeman@gmail.com>
2014-06-03 12:50:58 -0700

    Added start function

--- client/www/app/home/home.js
+++ client/www/app/home/home.js
@@ -1,5 +1,7 @@
 angular.module('home', [])
 
 .controller('HomeCtrl', ['$rootScope', '$scope', function($rootScope, $scope) {
-
+  $scope.start = function() {
+    $state.go('drugs.meds');
+  };
 }]);
