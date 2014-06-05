48b028e3b0b946e1845fd725982ff96c8e79caf3
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-05 16:01:54 -0700

    Updated gulpfile.

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,12 +1,9 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var sh = require('shelljs');
 var jshint = require('gulp-jshint');
-var karma = require('gulp-karma');
 
 var files = {
   scripts: ['./www/app/**/*.js', './tests/**/*.js'],
-  tests: ['./tests/**/*.js']
 };
 
 gulp.task('lint', function () {
@@ -16,10 +13,3 @@ gulp.task('lint', function () {
     .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('test', ['lint'], function () {
-  return gulp.src(files.tests)
-    .pipe(karma({
-      configFile: './karma.conf.js',
-      action: 'watch'
-    })).on('error', function (err) { throw err; });
-});
