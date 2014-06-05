738d13baead438eb130fb83408bb19300c77a371
mikefreeman <michael.p.freeman@gmail.com>
2014-06-04 22:10:57 -0700

    Added login/logout functions

--- client/www/app/home/home.js
+++ client/www/app/home/home.js
@@ -1,6 +1,17 @@
-angular.module('home', [])
+angular.module('home', ['twitterLib'])
 
-.controller('HomeCtrl', ['$rootScope', '$scope', '$state', function($rootScope, $scope, $state) {
+.controller('HomeCtrl', ['$rootScope', '$scope', '$state', 'TwitterLib', function($rootScope, $scope, $state, TwitterLib) {
+  $scope.login = function() {
+    TwitterLib.init().then(function() {
+      $state.go('home.start'); 
+    });
+  };
+
+  $scope.logout = function() {
+    TwitterLib.logOut()
+    $state.go('home.login');
+  };
+
   $scope.start = function() {
     $state.go('drugs.meds');
   };
